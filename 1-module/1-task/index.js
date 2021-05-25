09ac66ae87be78ff2da054699de6b740b9ae51ac
Marian Golub <golubmarian@gmail.com>
2021-05-25 22:06:36 +0300

    Add factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
   // ваш код...
+  let result = n;
+
+    if (n == 0 || n == 1) {
+     return 1; 
+    } else {
+      
+      for (let i = 1; i < n; i++) {
+        result = result * ( n - i ) ;
+      }
+    
+      return result;    
+    }
 }
