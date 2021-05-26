09579c36dee50e41c3d3d93c2f1864c473866f6f
Marian Golub <golubmarian@gmail.com>
2021-05-26 19:00:26 +0300

    Add isValid(name)

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,6 +11,9 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
+  if ((name !== null) && (name.indexOf(' ') == -1) && (name.length >= 4)) {
+    return true;
+  } else return false;
 }
 
 function sayHello() {
