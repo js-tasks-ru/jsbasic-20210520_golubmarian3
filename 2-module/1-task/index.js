53c72704d87e9e8ca724aa08661f1577f1e3b606
Marian Golub <golubmarian@gmail.com>
2021-05-31 16:13:20 +0300

    Add sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+
+    for (kay in salaries) {
+      if (isFinite(salaries[kay])) {
+        result = result + salaries[kay];
+      }
+    }
+  
+  return result;
 }
