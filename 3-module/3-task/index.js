b477aa7129600c4fe42a07796d9ab620d34041d3
Marian Golub <golubmarian@gmail.com>
2021-06-03 16:40:44 +0300

    Add camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,23 @@
 function camelize(str) {
   // ваш код...
+  //vol 1.0
+  // str = str.split('');   
+  
+  // str = str.map(function (item, index) {
+
+  //   if (str[index - 1] == '-') {
+  //     return item.toUpperCase();
+  //   } return item;     
+  
+  // });
+
+  // str = str.filter(item => item !== '-');            
+  // return str.join('');
+
+
+  // vol 1.1
+  str = str.split('');   
+  str = str.map((item, index) => (str[index - 1] == '-') ? item.toUpperCase() : item);
+  str = str.filter(item => item !== '-');
+  return str.join('');
 }
