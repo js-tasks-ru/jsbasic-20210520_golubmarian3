ab17b65f025698c9b7f3bdc6e0ceba62fad3d0c9
Marian Golub <golubmarian@gmail.com>
2021-06-04 19:12:09 +0300

    Add showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
   // ваш код...
+  users = users.filter(users => users.age <= age);
+  users = users.map(users => users.name + ', ' + users.balance);
+  return users.join('\n');
 }
