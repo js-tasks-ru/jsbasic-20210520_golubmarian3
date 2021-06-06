d5cc9d70d2b6b5c369bdb1ff2f927e1fd20b1ef2
Marian Golub <golubmarian@gmail.com>
2021-06-06 22:21:34 +0300

    Add makeFriendList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+
+  friends.forEach(item => {
+    ul.innerHTML += `<li>${item.firstName + ' ' + item.lastName}</li>`;
+  });
+
+  return ul;
 }
