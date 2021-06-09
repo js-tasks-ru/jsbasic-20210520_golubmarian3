d831f0281a2bc2fab9a90191812e234edfdbcc2d
Marian Golub <golubmarian@gmail.com>
2021-06-10 00:39:08 +0300

    Несколько тасков

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,12 +3,16 @@ function highlight(table) {
   for (let i = 1; i < table.rows.length; i++) {
 
     if (table.rows[i].cells[3].dataset.available) {
-      table.rows[i].cells[3].dataset.available == 'true' ? table.rows[i].className = 'available' : table.rows[i].className = 'unavailable';
+      table.rows[i].cells[3].dataset.available == 'true' 
+      ? table.rows[i].classList.add('available') 
+      : table.rows[i].classList.add('unavailable');
     } else {
       table.rows[i].setAttribute('hidden','');
     }
     
-    table.rows[i].cells[2].innerHTML == 'm' ? table.rows[i].classList.add('male') : table.rows[i].classList.add('female');
+    table.rows[i].cells[2].innerHTML == 'm' 
+    ? table.rows[i].classList.add('male') 
+    : table.rows[i].classList.add('female');
     
     if (table.rows[i].cells[1].innerHTML < 18) {
       table.rows[i].style = 'text-decoration: line-through';
