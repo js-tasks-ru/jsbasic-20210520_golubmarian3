1e7ca59128514f9d14096536c2372ec44fe32445
Marian Golub <golubmarian@gmail.com>
2021-06-23 15:25:34 +0300

    повтор

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,6 +1,6 @@
 function toggleText() {
   // ваш код...
-  document.querySelector('button').addEventListener('click', () => text.hidden == false ? text.hidden = true : text.hidden = false);
-  //document.querySelector('button').addEventListener('click', () => text.hidden = !text.hidden);
+  //document.querySelector('button').addEventListener('click', () => text.hidden == false ? text.hidden = true : text.hidden = false);
+  document.querySelector('button').addEventListener('click', () => text.hidden = !text.hidden);
   
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,55 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.product = product;    
+    this.elem = this.render();
+    this.event();
   }
+
+  render() {
+    let card = document.createElement('DIV');
+    card.className = 'card';
+    let cardTop = document.createElement('DIV');
+    cardTop.className = 'card__top';
+    let cardImage = document.createElement('IMG');
+    cardImage.className = 'card__image';
+    cardImage.src = `/assets/images/products/${this.product.image}`;
+    let cardPrice = document.createElement('SPAN');
+    cardPrice.className = 'card__price';
+    cardPrice.innerText = `€${this.product.price.toFixed(2)}`;
+    cardTop.append(cardImage,cardPrice);
+    let cardBody = document.createElement('DIV');
+    cardBody.className = 'card__body';
+    let cardTitle = document.createElement('DIV');
+    cardTitle.className = 'card__title';
+    cardTitle.innerText = this.product.name;
+    let cardButton = document.createElement('BUTTON');
+    cardButton.className = 'card__button';
+    cardButton.type = 'button';
+    let cardButtonImg = document.createElement('IMG');
+    cardButtonImg.alt = 'icon';
+    cardButtonImg.src = '/assets/images/icons/plus-icon.svg';
+    cardButton.append(cardButtonImg);
+    cardBody.append(cardTitle,cardButton);
+    card.append(cardTop, cardBody);
+    return card;
+  }
+
+  event() {
+    let event = new Event('product-add', {bubbles: true});
+    this.elem.dispatchEvent(event);
+    //console.log(this.product.id);
+
+    document.addEventListener('click', function(event) {
+      if (event.target.closest('.card__button')) {
+        console.log('кнопка');
+        console.log(event);
+        console.log(this.product.id);
+      } else {
+        return;
+      }      
+    });
+
+  }
+
 }
